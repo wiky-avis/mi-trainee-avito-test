7c9acc6a3c765f6df25e1c91c1fe8188793c4472
Виктория Аксентий <peta-holding@yandex.ru>
2021-06-14 16:13:52 +0300

    настройка отображения моделей в панели администратора

--- polls/admin.py
+++ polls/admin.py
@@ -1,2 +1,24 @@
 from django.contrib import admin
 
+from .models import Choice, Poll
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['title']
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        (None, {'fields': ['description']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Poll, PollAdmin)
+
+
